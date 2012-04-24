2746c0baebe62e908aed12e55cd0619029505530
tf5a11 <tf5a11@localhost>
2012-04-24 10:08:41 +0200

    Additional files.

---
+++ src/com/cmsm/metar/Configuration.java
@@ -0,0 +1,12 @@
+package com.cmsm.metar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Configuration extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.conf);
+	}
+}
